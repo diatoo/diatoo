6a420dd2fc2c8796ab7430e958e2566707d4eb32
malick <elmn64@yahoo.fr>
2012-11-25 21:35:38 +0100

    premier commit
    Signed-off-by: malick <elmn64@yahoo.fr>

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Accueil Diatoo</title>
+</head>
+<body style='background-color:'>
+	<CENTER><h1 style='font-size:60px'>Bienvenue sur le site de Diatoo</h1></CENTER>
+	<MARQUEE BEHAVIOR="alternate" DIRECTION="up"
+   BGCOLOR="white" WIDTH="100%" HEIGHT=200>
+<CENTER><h1 style='color:red'>Le site est en contruction</h1></CENTER>
+</MARQUEE>
+</body>
+</html>
